52a46d4421827dce2ec21ce6aca2ed528430404f
Saad Sohail <saad.sohail@smartechconsult.local>
2019-09-16 17:04:53 +0500

    🚧 Front End

--- FE/src/app/recruitments/add-recruitment/add-recruitment.component.ts
+++ FE/src/app/recruitments/add-recruitment/add-recruitment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-recruitment',
@@ -7,9 +10,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddRecruitmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private formBuilder: FormBuilder,private snackBar: MatSnackBar) {
+
+    this.formErrors = {
+      candidateName: {},
+      candidatePosition: {},
+      relevantEducation: {},
+      relevantJobPersistance: {},
+      relevantJobExperience: {},
+      appearance: {},
+      communicationSkills: {},
+      basicProgrammingSkills: {},
+      objectOrientedConcepts: {},
+      dataStructureConcepts: {},
+      algorithm: {},
+      designPattern: {},
+      programingLanguageSkills: {},
+      analyticalSkills: {},
+      understandingOfDevLifeCycle: {},
+      teamPlayerCapability: {},
+      teamLeadCapability: {},
+      suitabilityForPositionApplied: {},
+      scoreObtained:{},
+      overallEvaluation:{},
+      interviewerComments:{},
+      interviewerName:{},
+      interviewerDesignation: {},
+      recommendation: {},
+      proposedDesignation: {},
+      salary:{},
+      otherBenefits: {},
+      location: {},
+      date: {},
+      hiringAuthoritySignature: {}
+    
+    };
+   }
 
   ngOnInit() {
+    this.recruitmentForm = this.formBuilder.group({
+      candidateName: ['', [Validators.required, Validators.maxLength(50)]],
+      candidatePosition: ['', [Validators.required, Validators.maxLength(50)]],
+      relevantEducation: ['', [Validators.required]],
+      relevantJobPersistance: ['', [Validators.required]],
+      relevantJobExperience: ['', [Validators.required]],
+      appearance: ['', [Validators.required]],
+      communicationSkills: ['', [Validators.required]],
+      basicProgrammingSkills: ['', [Validators.required]],
+      objectOrientedConcepts: ['', [Validators.required]],
+      dataStructureConcepts: ['', [Validators.required]],
+      algorithm: ['', [Validators.required]],
+      designPattern: ['', [Validators.required]],
+      programingLanguageSkills: ['', [Validators.required]],
+      analyticalSkills: ['', [Validators.required]],
+      understandingOfDevLifeCycle: ['', [Validators.required]],
+      teamPlayerCapability: ['', [Validators.required]],
+      teamLeadCapability: ['', [Validators.required]],
+      suitabilityForPositionApplied: ['', [Validators.required]],
+      scoreObtained: ['', [Validators.required, Validators.pattern('^[0-9-]*$'), Validators.maxLength(50)]],
+      overallEvaluation: ['', [Validators.required]],
+      interviewerComments: ['', [Validators.required, Validators.maxLength(500)]],
+      interviewerName: ['', [Validators.required, Validators.maxLength(50)]],
+      interviewerDesignation: ['', [Validators.required, Validators.maxLength(50)]],
+      recommendation: ['', [Validators.required]],
+      proposedDesignation: ['', [Validators.required, Validators.maxLength(50)]],
+      salary: [0, [Validators.required, Validators.pattern('^[0-9-]*$'), Validators.maxLength(50)]],
+      otherBenefits: ['', [Validators.required, Validators.maxLength(500)]],
+      location: ['', [Validators.required, Validators.maxLength(50)]],
+      date: ['', [Validators.required, Validators.maxLength(50)]],
+      hiringAuthoritySignature: [''],
+    });
+  }
+
+  recruitmentForm: FormGroup;
+  formErrors: any;
+
+  onFormValuesChanged() {
+    for (const field in this.formErrors) {
+      if (!this.formErrors.hasOwnProperty(field)) {
+        continue;
+      }
+
+      this.formErrors[field] = {};
+      const control = this.recruitmentForm.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        this.formErrors[field] = control.errors;
+      }
+    }
+  }
+
+  addRecruitment(){
+    console.log(this.recruitmentForm.value);
   }
 
 }
---
+++ FE/src/app/services/recruitments.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse, HttpResponseBase } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs/Rx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecruitmentsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  rest_api = 'http://localhost:3000';
+  recruitment = new BehaviorSubject<any>({});
+  selected_recruitment = this.recruitment.asObservable();
+
+  change_recruitment(recruitment) {
+    this.recruitment.next(recruitment);
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  getObservableRecruitments(): Observable<any> {
+    return this.http.get(this.rest_api + '/recruitments');
+  }
+
+  getrecruitment(recruitment): Observable<any> {
+    return this.http.post(this.rest_api + '/recruitments/id', recruitment, this.httpOptions);
+  }
+
+  addrecruitment(recruitment): Observable<any> {
+    return this.http.post(this.rest_api + '/recruitments', recruitment, this.httpOptions);
+  }
+
+  deleterecruitment(recruitment): Observable<any> {
+    return this.http.post(this.rest_api + '/recruitments/delete', recruitment, this.httpOptions);
+  }
+
+  editrecruitment(recruitment): Observable<any> {
+    return this.http.put(this.rest_api + '/recruitments', recruitment, this.httpOptions);
+  }
+
+}
